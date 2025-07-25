ab155fe7d07558bdf8e78f8e50d2527749d8ba10
Abdul Wahab <abdulwahab@digicopsolutions.com>
2025-07-25 22:06:04 +0500

    Update portfolio files

--- src/app/BackgroundWrapper.tsx
+++ src/app/BackgroundWrapper.tsx
@@ -7,24 +7,19 @@ export default function BackgroundWrapper({ children }: { children: React.ReactN
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    // Hide loader after 2 seconds
     const timer = setTimeout(() => {
       setLoading(false);
     }, 2000);
-
     return () => clearTimeout(timer);
   }, []);
 
   return (
     <>
-      {/* Loader for 2s */}
       {loading && (
         <div className="fixed inset-0 z-[9999] bg-black flex items-center justify-center">
           <Loader />
         </div>
       )}
-
-      {/* Content after loader */}
       {!loading && children}
     </>
   );
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import BackgroundWrapper from './BackgroundWrapper';
 import Footer from './components/footer';
 import WhatsappChat from './components/whatsapp';
 
-
 export const metadata = {
   title: 'Abdul Wahab Portfolio',
   description: 'Built with Next.js',
@@ -12,10 +11,9 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body className="min-h-screen overflow-x-hidden text-white ">
+      <body className="min-h-screen overflow-x-hidden text-white">
         <BackgroundWrapper>
           {children}
-
           <WhatsappChat />
           <Footer />
         </BackgroundWrapper>
